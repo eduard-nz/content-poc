1b69bdbb38fc47dff32aadd9f588199a1b775c50
Eduard <eduard@snz.com>
2024-06-04 09:22:28 +0000

    Minor formatting in markdown rendering

--- publishing/components/renderComponents.tsx
+++ publishing/components/renderComponents.tsx
@@ -4,8 +4,8 @@ import { DropBox, DropBoxProps } from './dropBox';
 export const renderComponents: Components<{
   dropBox: DropBoxProps;
 }> = {
-  ol: (props) => <ol className="list-decimal pl-5" {...props} />,
-  ul: (props) => <ul className="list-disc pl-5" {...props} />,
+  ol: (props) => <ol className="mb-8 mt-8 list-decimal pl-5" {...props} />,
+  ul: (props) => <ul className="mb-8 mt-8 list-disc pl-5" {...props} />,
   li: (props) => <li className="mt-1" {...props} />,
   p: (props) => <p className="mt-4" {...props} />,
   dropBox: (props: DropBoxProps) => <DropBox {...props} />,
